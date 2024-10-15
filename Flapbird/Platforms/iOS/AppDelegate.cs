6efe6212bd9842c906667101c1a79d94531c03e6
Manu <emanuelebrumrossi0@gmail.com>
2024-10-15 07:55:24 -0300

    Início do jogo

---
+++ Flapbird/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Flapbird;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
