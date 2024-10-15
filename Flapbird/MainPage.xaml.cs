9d6eb0dd67693945a0e56f7dcdec62d42a684175
Manu <emanuelebrumrossi0@gmail.com>
2024-10-15 09:01:16 -0300

    Montagem do jogo =)!

--- Flapbird/MainPage.xaml.cs
+++ Flapbird/MainPage.xaml.cs
@@ -2,23 +2,98 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	const int gravidade = 1;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = false;
+	double larguraJanela = 0;
+	double AlturaJanela = 0;
+	int velocidade = 20;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
+	void AplicaGravidade()
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Passaro.TranslationY += gravidade;
 	}
+	public async void Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				estaMorto = true;
+				FrameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+
+	void Oi(object s, TappedEventArgs e)
+	{
+		FrameGameOver.IsVisible = false;
+		estaMorto = false;
+		Inicializar();
+		Desenha();
+	}
+
+	void Inicializar()
+	{
+		Passaro.TranslationY = 0;
+	}
+
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela = w;
+		AlturaJanela = h;
+	}
+
+	void GerenciaCanos()
+	{
+		imgcanocima.TranslationX -= velocidade;
+		imgcanobaixo.TranslationX -= velocidade;
+		if (imgcanobaixo.TranslationX < -larguraJanela)
+		{
+			imgcanobaixo.TranslationX = 4;
+			imgcanocima.TranslationX = 4;
+		}
+	}
+	bool VerificaColisao()
+	{
+		if (!estaMorto)
+		{
+			if (VerificaColisaoTeto() ||
+			VerificaColisaoChao())
+			{
+				return true;
+			}
+
+		}
+		return false;
+	}
+	bool VerificaColisaoTeto()
+	{
+		var minY = AlturaJanela / 2;
+		if (Passaro.TranslationY <= minY)
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisaoChao()
+	{
+		var maxY = AlturaJanela / 2 - chao.HeightRequest;
+		if (Passaro.TranslationY >= maxY)
+			return true;
+		else
+			return false;
+
+	}
+
 }
 
